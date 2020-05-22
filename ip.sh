229131a8bb09d6536bfdaff4651dbe1b32decb60
mog54 <petitdalouzy@hotmail.fr>
2020-05-22 12:31:56 +0200

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -26,7 +26,7 @@ fi
 
 # Check incomming 9090/tcp from evrywhere
 echo "Check if incomming 9090/tcp from evrywhere exist..."
-if ! /sbin/iptables -n -L | grep -iE " 9090P " >/dev/null 2>&1 ; then
+if ! /sbin/iptables -n -L | grep -iE " 9090 " >/dev/null 2>&1 ; then
 echo "it did NOT exist, creating rule..."
 # creating rule
 /sbin/iptables -A INPUT -p tcp --dport 9090 -j ACCEPT
