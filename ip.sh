1daeaaa223d9a8efe02fd7bed35522158a43db73
mog54 <petitdalouzy@hotmail.fr>
2023-02-11 15:23:00 +0100

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -7,8 +7,8 @@ DYNIP=$(host $DYNHOST | grep -iE "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" |cut -f4 -d' '
 # CHECK NAT
 IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 if [[ "$IP" = "" ]]; then
-		echo '1'
-		IP=$(wget -qO- api.ipify.org)
+                echo '1'
+                IP=$(wget -qO- api.ipify.org)
 fi
 
 # Exit if invalid IP address is returned
@@ -29,23 +29,37 @@ echo "Check if incomming 9090/tcp from evrywhere exist..."
 if ! /sbin/iptables -n -L | grep -iE "9090" >/dev/null 2>&1 ; then
 echo "it did NOT exist, creating rule..."
 # creating rule
-/sbin/iptables -A INPUT -p tcp --dport 9090 -j ACCEPT
+#/sbin/iptables -A INPUT -p tcp --dport 9090 -j ACCEPT
 /sbin/iptables -A INPUT -p tcp --dport 45000 -j LOG --log-level debug --log-prefix "45000/TCP "
 /sbin/iptables -A INPUT -p tcp --dport 45000 -j ACCEPT
 /sbin/iptables -A INPUT -p udp --dport 45000 -j LOG --log-level debug --log-prefix "45000/UDP "
 /sbin/iptables -A INPUT -p udp --dport 45000 -j ACCEPT
+#/sbin/iptables -A INPUT -p tcp --dport 32400 -j LOG --log-level debug --log-prefix "32400/TCP "
+#/sbin/iptables -A INPUT -p tcp --dport 32400 -j ACCEPT
+/sbin/iptables -A INPUT -p tcp --dport 32400 -m set --match-set PLEX src -j LOG --log-level debug --log-prefix "PLEX "
+/sbin/iptables -A INPUT -p tcp --dport 32400 -m set --match-set PLEX src -j ACCEPT
 /sbin/iptables -A INPUT -p tcp --dport 443 -m set --match-set CLOUDFLARE src -j LOG --log-level debug --log-prefix "HTTPS "
 /sbin/iptables -A INPUT -p tcp --dport 443 -m set --match-set CLOUDFLARE src -j ACCEPT
+/sbin/iptables -A INPUT -s 0.0.0.0/32 -j LOG --log-level debug --log-prefix "TSI "
+/sbin/iptables -A INPUT -s 0.0.0.0/32 -j ACCEPT
+#/sbin/iptables -A INPUT -s 195.137.186.65/32 -p icmp -j LOG --log-level debug --log-prefix "PING-TSI "
+#/sbin/iptables -A INPUT -s 195.137.186.65/32 -p icmp -j ACCEPT
+#/sbin/iptables -A INPUT -s 95.211.81.67/32 -j LOG --log-level debug --log-prefix "SEEDHOST "
+#/sbin/iptables -A INPUT -s 95.211.81.67/32 -j ACCEPT
+/sbin/iptables -A INPUT -s 82.66.125.140/32 -p tcp --dport 32400 -j LOG --log-level debug --log-prefix "PARENT "
+/sbin/iptables -A INPUT -s 82.66.125.140/32 -p tcp --dport 32400 -j ACCEPT
+#/sbin/iptables -A INPUT -s 37.48.65.162/32 -p icmp -j LOG --log-level debug --log-prefix "PING SEEDHOST "
+#/sbin/iptables -A INPUT -s 37.48.65.162/32 -p icmp -j ACCEPT
 fi
 
 
 # Check Allow All outgoing
 echo "Check if Allow All outgoing exist..."
-if ! /sbin/iptables -n -L | grep -iE "ACCEPT" | grep -iE "0.0.0.0/0" | grep -iE "all" >/dev/null 2>&1 ; then
+if ! /sbin/iptables -n -L | grep -iE "ACCEPT" | grep -iE "0.0.0.0/0" | grep -iE "state RELATED,ESTABLISHED" | grep -iE "all" >/dev/null 2>&1 ; then
 echo "it did NOT exist, creating rule..."
 # creating rule
-/sbin/iptables -I OUTPUT -o enp4s0 -d 0.0.0.0/0 -j ACCEPT
-/sbin/iptables -I INPUT -i enp4s0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+/sbin/iptables -I OUTPUT -o enp3s0 -d 0.0.0.0/0 -j ACCEPT
+/sbin/iptables -I INPUT -i enp3s0 -m state --state ESTABLISHED,RELATED -j ACCEPT
 fi
 
 # Check Block All incoming
@@ -72,8 +86,12 @@ echo "it did NOT match, creating chain and rules..."
 /sbin/iptables -F $DYNHOST >/dev/null 2>&1
 /sbin/iptables -I $DYNHOST -s $DYNIP -p tcp --dport 22 -j ACCEPT
 /sbin/iptables -I $DYNHOST -s $DYNIP -p icmp -j ACCEPT
-#/sbin/iptables -I $DYNHOST -s $DYNIP -p tcp --dport 111 -j ACCEPT
-
+/sbin/iptables -I $DYNHOST -s $DYNIP -p tcp --dport 9090 -j ACCEPT
+/sbin/iptables -I $DYNHOST -s $DYNIP -p tcp --dport 443 -j ACCEPT
+/sbin/iptables -I $DYNHOST -s $DYNIP -p tcp --dport 8384 -j ACCEPT
+/sbin/iptables -I $DYNHOST -s $DYNIP -p udp --dport 22000 -j ACCEPT
+/sbin/iptables -I $DYNHOST -s $DYNIP -p tcp --dport 32400 -j ACCEPT
+/sbin/iptables -I $DYNHOST -s $DYNIP -p tcp --dport 80 -j ACCEPT
 
 # Add chain to INPUT filter if it doesn't exist
 if ! /sbin/iptables -C INPUT -t filter -j $DYNHOST >/dev/null 2>&1 ; then
